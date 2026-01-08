e3909b906af0e8883e5541d6f0973eca680b4b06
Damian Holona <damianholona074@gmail.com>
2026-01-08 15:47:04 +0100

    + init

---
+++ src/main/java/com/dname074/loyalty_program_system/LoyaltyProgramSystemApplication.java
@@ -0,0 +1,13 @@
+package com.dname074.loyalty_program_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProgramSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProgramSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dname074/loyalty_program_system/LoyaltyProgramSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dname074.loyalty_program_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoyaltyProgramSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
