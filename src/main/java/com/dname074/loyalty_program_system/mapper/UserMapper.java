b4f26b718b6fec733cf1885dc521b69c1a69b790
Damian Holona <damianholona074@gmail.com>
2026-01-10 02:21:21 +0100

    + [LOY-03] add user mapper

---
+++ src/main/java/com/dname074/loyalty_program_system/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.dname074.loyalty_program_system.mapper;
+
+import com.dname074.loyalty_program_system.dto.CreateUserCommand;
+import com.dname074.loyalty_program_system.dto.UserDto;
+import com.dname074.loyalty_program_system.model.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+    UserDto toDto(User user);
+    User toEntity(CreateUserCommand user);
+}
